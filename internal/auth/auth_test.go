44c74cd864dcdb03c5fce6273485d7f94cb1377b
WEku <roba.weku@gmail.com>
2025-08-13 10:53:44 +0200

    Break code to test CI failure

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -28,7 +28,7 @@ func TestGetAPIKey(t *testing.T) {
 			wantErrSubstr: "API key is missing",
 		},
 		{
-			name:       "wrong key",
+			name:       "Valid key",
 			authHeader: "ApiKey my-secret-key",
 			wantKey:    "my-secret-key",
 		},
